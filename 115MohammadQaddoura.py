f73dd8398eac9234dd55b10d6a19d88d68037fc7
Mohammad78205 <55498351+Mohammad78205@users.noreply.github.com>
2019-09-20 10:30:46 -0400

    v4

--- 115MohammadQaddoura.py
+++ 115MohammadQaddoura.py
@@ -1,19 +1,66 @@
-#   a115_buggy_image.py
+#   a115_ladybug.py
 import turtle as trtl
 
-drawer = trtl.Turtle()
-drawer.pensize(40)
-drawer.circle(20)
-legs = 8
-leg_length = 70
-leg_space = 360 / legs
-drawer.pensize(5)
-count = 0
-while (count < legs):
-  drawer.goto(0,20)
-  drawer.setheading(leg_space*count)
-  drawer.forward(leg_length)
-  count = count + 1
-drawer.hideturtle()
+# make ladybug legs
+ladybug = trtl.Turtle()
+w = 6
+y = 60
+z = 360 / w
+ladybug.pensize(5)
+n = 0
+while (n < w):
+  ladybug.goto(0,-35)
+  ladybug.setheading(z*n)
+  ladybug.forward(y)
+  n = n + 1
+ladybug.hideturtle()
+# create ladybug head
+ladybug = trtl.Turtle()
+ladybug.pensize(40)
+ladybug.circle(5)
+
+# and body
+ladybug.penup()
+ladybug.goto(0, -55)
+ladybug.color("red")
+ladybug.pendown()
+ladybug.pensize(40)
+ladybug.circle(20)
+ladybug.setheading(270)
+ladybug.color("black")
+ladybug.penup()
+ladybug.goto(0, 5)
+ladybug.pensize(2)
+ladybug.pendown()
+ladybug.forward(75)
+
+# config dots
+num_dots = 1
+xpos = -20
+ypos = -55
+ladybug.pensize(10)
+
+# draw two sets of dots
+num_dots = 0
+while (num_dots < 2 ):
+  ladybug.penup()
+  ladybug.goto(xpos, ypos)
+  ladybug.pendown()
+  ladybug.circle(3)
+  ladybug.penup()
+  ladybug.goto(xpos + 30, ypos + 20)
+  ladybug.pendown()
+  ladybug.circle(2)
+
+  # position next dots
+ 
+  ypos = xpos + -10
+  xpos = xpos
+  num_dots = num_dots + 1
+ 
+ 
+ladybug.hideturtle()
+
 wn = trtl.Screen()
 wn.mainloop()
+
